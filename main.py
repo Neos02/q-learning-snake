df3a6026c0635b316a0fb2aac0b7c5015e0b981f
Nicholas Kennedy <nmk10062@gmail.com>
2025-09-09 13:47:02 -0400

    Create game class

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import sys
-import time
 import pygame
-
-from pygame.locals import *
 
 pygame.init()
 
@@ -29,42 +25,6 @@ DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Snake')
 
 if __name__ == "__main__":
-    from apple import Apple
-    from player import Player
-
-    apple = Apple()
-    player = Player()
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-                pygame.quit()
-                sys.exit()
-
-        player.move()
-
-        if pygame.sprite.collide_rect(player, apple):
-            apple = Apple()
-            player.length += 1
-
-        DISPLAYSURF.fill(BLACK)
-        apple.draw(DISPLAYSURF)
-        player.draw(DISPLAYSURF)
-
-        score = FONT_SMALL.render("Score: " + str(player.length), True, WHITE)
-        DISPLAYSURF.blit(score, (SCREEN_WIDTH - 100, 10))
-
-        if player.rect.left < 0 or player.rect.right > SCREEN_WIDTH \
-                or player.rect.top < 0 or player.rect.bottom > SCREEN_HEIGHT \
-                or player.self_collide():
-            game_over = FONT_LARGE.render("Game Over", True, WHITE)
-            DISPLAYSURF.fill(RED)
-            DISPLAYSURF.blit(game_over, (SCREEN_WIDTH / 2 - game_over.get_width() / 2, SCREEN_HEIGHT / 2 - game_over.get_height() / 2))
-
-            pygame.display.update()
-            time.sleep(2)
-            pygame.quit()
-            sys.exit()
-
-        pygame.display.update()
-        CLOCK.tick(FPS)
+    from game import Game
+    game = Game()
+    game.run()
