60d47cb1950c724b5396dd72257be5501a6f6a4e
Nicholas Kennedy <nmk10062@gmail.com>
2025-09-10 09:09:51 -0400

    Clean up code

--- player.py
+++ player.py
@@ -6,29 +6,31 @@ from main import TILE_WIDTH, TILE_HEIGHT, GREEN, SCREEN_WIDTH, SCREEN_HEIGHT, RO
 
 class Player(pygame.sprite.Sprite):
 
-    def __init__(self):
+    def __init__(self, is_agent=False):
         super().__init__()
         self.body = [pygame.Rect(TILE_WIDTH * COLUMN_COUNT // 2, TILE_HEIGHT * ROW_COUNT // 2, TILE_WIDTH, TILE_HEIGHT)]
         self.rect = self.body[0]
         self.velocity = (0, 0)
         self.length = 5
+        self.is_agent = is_agent
 
     def move(self):
         pressed_keys = pygame.key.get_pressed()
 
-        if self.velocity[0] == 0:
-            if pressed_keys[K_a]:
-                self.velocity = (-TILE_WIDTH, 0)
+        if not self.is_agent:
+            if self.velocity[0] == 0:
+                if pressed_keys[K_a]:
+                    self.velocity = (-TILE_WIDTH, 0)
 
-            if pressed_keys[K_d]:
-                self.velocity = (TILE_WIDTH, 0)
+                if pressed_keys[K_d]:
+                    self.velocity = (TILE_WIDTH, 0)
 
-        if self.velocity[1] == 0:
-            if pressed_keys[K_w]:
-                self.velocity = (0, -TILE_HEIGHT)
+            if self.velocity[1] == 0:
+                if pressed_keys[K_w]:
+                    self.velocity = (0, -TILE_HEIGHT)
 
-            if pressed_keys[K_s]:
-                self.velocity = (0, TILE_HEIGHT)
+                if pressed_keys[K_s]:
+                    self.velocity = (0, TILE_HEIGHT)
 
         if self.velocity != (0, 0):
             self.add_segment()
